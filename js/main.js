6ec7ef953cc6b32b123fef7e9e7b29c61bc9fe82
Sander <svdhoogen1999@gmail.com>
2019-05-07 12:28:40 +0200

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
+// Function which handles keypress and checks if it should be handled or not.
+function isNumberKey(e) {
+    var key = getKeyEvent(e); // Get the keycode, with added compatibility for IE and other browsers.
+
+    if (key < 32 || (key > 47 && key < 58)) {
+        return true;
+    } else {
+        return false;
+    } // If the key is a functional key(backspace, tab etc.), or is a number(48-57), handle keypress, otherwise, don't handle keypress.
+}
+
+// Event which ensures compatability between different browsers for keyevent.
+function getKeyEvent(e) {
+    var keyNum;
+    if (window.event) {
+        keyNum = e.keyCode
+    } else if (e.which) {
+        keyNum = e.which
+    }
+    return keyNum; // Checks to see which browser it is on and performs logic to return keyNum for that browser, for compat.
+}
+
+// Verifies input on maxNum textbox, makes sure value is above 10 and string length doesn't exceed 3 characters, and update slider only if it conforms to 10 - 999 range.
+function maxNumVerify(minInputNum) {
+    var inputNum = document.getElementById("inputNum");
+    var num = inputNum.value; // Define element and get string to variable num..
+
+    if (num == "") {
+        inputNum.value = minInputNum;
+    } // If field is empty, fill it with minimum expected input.
+    while (num.length > 3) {
+        num = maxNum.substring(0, num.length - 1);
+        inputNum.value = num;
+    } // while maxNum exceeds 3 characters, remove last digit from string.
+
+    var intNum = parseInt(inputNum.value);
+    if (intNum < minInputNum) {
+        inputNum.value = minInputNum;
+    } else if (intNum > 999) {
+        inputNum.value = 999;
+    } // If num is exceeds boundary, fill it with boundary maximums.
+    var intNum = parseInt(inputNum.value);
+    if (intNum > 9 && intNum < 1000 && minInputNum == 10) {
+        document.getElementById("maxNumSli").value = intNum;
+    } // Update slider if is present and num is conforms to boundary.
+}
+
 $(document).ready(function () {
-    // Function which handles keypress and checks if it should be handled or not.
-    function isNumberKey(e) {
-        var key = getKeyEvent(e); // Get the keycode, with added compatibility for IE and other browsers.
-
-        if (key < 32 || (key > 47 && key < 58)) {
-            return true;
-        } else {
-            return false;
-        } // If the key is a functional key(backspace, tab etc.), or is a number(48-57), handle keypress, otherwise, don't handle keypress.
-    }
-
-    // Event which ensures compatability between different browsers for keyevent.
-    function getKeyEvent(e) {
-        var keyNum;
-        if (window.event) {
-            keyNum = e.keyCode
-        } else if (e.which) {
-            keyNum = e.which
-        }
-        return keyNum; // Checks to see which browser it is on and performs logic to return keyNum for that browser, for compat.
-    }
-
-    // Verifies input on maxNum textbox, makes sure value is above 10 and string length doesn't exceed 3 characters, and update slider only if it conforms to 10 - 999 range.
-    function maxNumVerify(minInputNum) {
-        var inputNum = document.getElementById("inputNum");
-        var num = inputNum.value; // Define element and get string to variable num..
-
-        if (num == "") {
-            inputNum.value = minInputNum;
-        } // If field is empty, fill it with minimum expected input.
-        while (num.length > 3) {
-            num = maxNum.substring(0, num.length - 1);
-            inputNum.value = num;
-        } // while maxNum exceeds 3 characters, remove last digit from string.
-
-        var intNum = parseInt(inputNum.value);
-        if (intNum < minInputNum) {
-            inputNum.value = minInputNum;
-        } else if (intNum > 999) {
-            inputNum.value = 999;
-        } // If num is exceeds boundary, fill it with boundary maximums.
-        var intNum = parseInt(inputNum.value);
-        if (intNum > 9 && intNum < 1000 && minInputNum == 10) {
-            document.getElementById("maxNumSli").value = intNum;
-        } // Update slider if is present and num is conforms to boundary.
-    }
-
     // If slider is present on page, add an eventlistener that will update the value in maxNum text input to slider value every time value changes.
     if (document.getElementById("maxNumSli") != null) {
         document.getElementById("maxNumSli").addEventListener("input", function () {
@@ -76,7 +76,7 @@ $(document).ready(function () {
     function showHint() {
         $.get('inc/getHighScores.php', function (scores) {
             console.log(scores);
-            document.getElementById("highscore1").innerHTML = "1: <strong>" + scores["1"].scoreNum + "</strong>, " + scores["1"].scoreName;
+            $("#highscore1").html("1: <strong>" + scores["1"].scoreNum + "</strong>, " + scores["1"].scoreName);
             document.getElementById("highscore2").innerHTML = "2: <strong>" + scores["2"].scoreNum + "</strong>, " + scores["2"].scoreName;
             document.getElementById("highscore3").innerHTML = "3: <strong>" + scores["3"].scoreNum + "</strong>, " + scores["3"].scoreName;
             document.getElementById("highscore4").innerHTML = "4: <strong>" + scores["4"].scoreNum + "</strong>, " + scores["4"].scoreName;
